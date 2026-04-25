bef5989dbe2a62060009e92a062b03abdad23026
Latikajakhra <latikajakhra1@gmail.com>
2026-04-25 17:46:33 +0530

    Initial blog API project

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models, schemas
+from database import engine, SessionLocal, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Create Blog
+@app.post("/blogs/")
+def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, content=blog.content)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+# Get all blogs
+@app.get("/blogs/")
+def get_blogs(db: Session = Depends(get_db)):
+    return db.query(models.Blog).all()
+
+# Get single blog
+@app.get("/blogs/{blog_id}")
+def get_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return blog
+
+# Delete blog
+@app.delete("/blogs/{blog_id}")
+def delete_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    db.delete(blog)
+    db.commit()
+    return {"message": "Deleted successfully"}
+
